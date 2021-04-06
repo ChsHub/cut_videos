3fa71b826fdb27a49b0b41c99b87c02e30674667
ChsHub <christian1193@web.de>
2021-04-06 16:25:44 +0200

    task_gif.py: convert to one type of gif;

--- cut_videos/model/task_gif.py
+++ cut_videos/model/task_gif.py
@@ -11,20 +11,14 @@ class TaskGif(Task):
     def _convert(self, i_file, o_file, frame_count):
         self._set_total_frames(frame_count)
         with TemporaryDirectory() as palette_dir:
-            # generate palette
-            with Timer('GENERATE PALETTE', log_function=info):
-                palette = palette_dir + '/palette.png'
-                self._run_command(file=i_file,
-                                  command=' -vf "scale=<res>:flags=lanczos,palettegen"',
-                                  new_file=palette)
+            # Generate palette
+            palette = palette_dir + '/palette.png'
+            self._run_command(file=i_file,
+                              command=' -vf "scale=<res>:flags=lanczos,palettegen"',
+                              new_file=palette)
             if not exists(palette):
                 error('No Palette')
                 return
-
-            for mode, suffix in [
-                (':flags=lanczos,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle', "_bayer.gif"),
-                (':flags=lanczos,paletteuse=dither=none', "_default.gif"),
-                ('', '_no_pal.gif')]:
-                self._run_command(file=i_file + '" -i "' + palette,
-                                  command=' -lavfi "scale=<res>%s"' % mode,
-                                  new_file=o_file + suffix)
+            self._run_command(file=i_file + '" -i "' + palette,
+                              command=' -lavfi "scale=<res>:flags=lanczos,paletteuse=dither=bayer:bayer_scale=5:diff_mode=rectangle"',
+                              new_file=o_file + '_bayer.gif')
