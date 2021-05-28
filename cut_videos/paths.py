454fa9e5e6ebe1621e92e2e1d55e373460d73f68
ChsHub <christian1193@web.de>
2021-05-28 02:34:38 +0200

    paths.py: remove paths;

--- cut_videos/paths.py
+++ cut_videos/paths.py
@@ -1,5 +1,2 @@
-from os.path import abspath
-
-ffmpeg_path = abspath('lib\\ffmpeg\\bin\\ffmpeg.exe')
-ffprobe_path = abspath('lib\\ffmpeg\\bin\\ffprobe.exe')
-
+ffmpeg_path = 'ffmpeg'
+ffprobe_path = 'ffprobe'
