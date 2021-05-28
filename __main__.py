9c0bf5768a68a327d4ac885f340765ef23bbfbdd
ChsHub <christian1193@web.de>
2021-05-28 02:31:55 +0200

    __main__.py: remove check, use ffmpeg from PATH;

--- __main__.py
+++ __main__.py
@@ -1,6 +1,3 @@
-from logging import info
-from os.path import exists
-from cut_videos.paths import ffmpeg_path, ffprobe_path
 from logger_default import Logger
 from wx import App
 
@@ -8,10 +5,6 @@ from cut_videos.view.window import Window
 
 if __name__ == "__main__":
     with Logger(debug=True):
-        if not exists(ffmpeg_path) or not exists(ffprobe_path):
-            info('ffmpeg not found')
-            raise FileNotFoundError
-
         app = App(False)
         frame = Window()
         frame.Show()
